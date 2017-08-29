42ad3a5ec36ba53b18c4a711b87646b3ea1c9f67
Ross Kiser <kiserr10@gmail.com>
2017-08-28 19:45:00 -0600

    fixed structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Passions from './components/Passions';
+import {
+	BroswerRouter as Router,
+	Route,
+	Link
+} from 'react-router-dom';
 
 
 class App extends Component {
@@ -27,4 +36,15 @@ class App extends Component {
 	}
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Router>
+		<div>
+			<Route path='/' component={App} />
+			<Route path='/about' component={About} />
+			<Route path='/projects' component={Projects} />
+			<Route path='/contact' component={Contact} />
+			<Route path='/passions' component={Passions} />
+		</div>
+	</Router>
+
+, document.getElementById('root'));
