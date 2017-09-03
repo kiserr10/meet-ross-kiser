db8d9be3f105415c2aab5ddeeacf5d4b51a1d565
Ross Kiser <kiserr10@gmail.com>
2017-09-03 12:26:45 -0600

    about page done and projects page layout created

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,31 @@ import{
 class About extends Component {
 	render(){
 		return(
-			<div>
-
+			<div className="about-main">
+				<div className="about-box-1 animated fadeInUp">
+					<h1>About Me:</h1>
+				</div>
+				<div className="about-box-2">
+					<div className="about-nav-1">
+						<li className="about-home-link"><Link to='/'>Home</Link></li>
+						<li className="about-projects-link"><Link to='/projects'>Tech Projects</Link></li>
+						<li className="about-music-link"><Link to='/music'>Music</Link></li>
+						<li className="about-contact-link"><Link to='/contact'>Contact</Link></li>
+					</div>
+					<div className="about-nav-2">
+						<img className="main-about-logo animated zoomIn" src={require('../images/rk-logo-fix.png')}/>
+					</div>
+				</div>
+				<div className="about-box-3">
+					<div className="my-bio">
+						<p>	Long before I became a web developer, technology has been central to my creative practice. I self-taught my way into audio engineering and started my own electronic music record label which has gone on to sign several internationally acclaimed acts. After developing this part of my life for over half a decade, I encountered the world of code. A deeper dimension of technology with which I instantly clicked, it was the perfect alignment of design, creativity, and problem solving that I had been seeking. In 2017, I decided to dive in fully and immerse myself in the culture of development by attending Galvanize. I hope to use these new skillsets to have a creative avenue to influence the worlds of my other passions, such as music, rock climbing, and food.
+						</p>
+					</div>
+				</div>
+				<div className="about-box-4">
+					<img className="main-prof-pic" src={require('../images/headshot.jpg')}/>
+				</div>
+				<div className="about-box-5"></div>
 			</div>
 		);
 	}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,8 +9,29 @@ import{
 class Projects extends Component {
 	render(){
 		return(
-			<div>
-
+			<div className="main-body-projects">
+				<div className="projects-box-1 animated fadeInUp">
+					<h1>Tech Projects:</h1>
+				</div>
+				<div className="projects-box-2">
+					<div className="about-nav-1">
+						<li className="about-home-link"><Link to='/'>Home</Link></li>
+						<li className="about-projects-link"><Link to='/projects'>Tech Projects</Link></li>
+						<li className="about-music-link"><Link to='/music'>Music</Link></li>
+						<li className="about-contact-link"><Link to='/contact'>Contact</Link></li>
+					</div>
+					<div className="about-nav-2">
+						<img className="main-about-logo animated flipInY" src={require('../images/rk-logo-fix.png')}/>
+					</div>
+				</div>
+				<div className="projects-box-3"></div>
+				<div className="projects-box-4"></div>
+				<div className="projects-box-5"></div>
+				<div className="projects-box-6"></div>
+				<div className="projects-box-7"></div>
+				<div className="projects-box-8"></div>
+				<div className="projects-box-9"></div>
+				<div className="projects-box-10"></div>
 			</div>
 		);
 	}
