20613cf3070db235f627fe49000a1256b5cb34c4
Ross Kiser <kiserr10@gmail.com>
2017-10-10 14:22:26 -0600

    fixed image size issue

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,22 +8,43 @@ import{
 } from 'react-router-dom';
 
 class Contact extends Component {
+	constructor(props){
+		super(props);
+		this.onMouseOver = this.onMouseOver.bind(this);
+		this.onMouseLeave = this.onMouseLeave.bind(this);
+		this.state = {
+			text: ''
+		};
+	}
+
+	onMouseOver(){
+		this.setState({
+			text: 'ross'
+		});
+		console.log(this.state.text);
+	}
+	onMouseLeave(){
+		this.setState({
+			text: ''
+		});
+	}
+
 	render(){
 		return(
 			<div>
 				<Header2 />
 				<div className="contact-main-body">
 					<div className="contact-box-3">
-						<a href="mailto:kiserr10@gmail.com"><img className="email-logo" src={require('../images/003-gmail-envelope.png')} height="65px" /></a>
-						<img className="linkedIn-logo" src={require('../images/005-linkedin-logo-1.png')} height="65px" />
-						<img className="github-logo" src={require('../images/github-logo.png')} height="65px" />
-						<img className="facebook-logo" src={require('../images/002-facebook-black-social-button-circle.png')} height="65px" />
-						<img className="insta-logo" src={require('../images/004-instagram-logo.png')} height="65px" />
-						<img className="beatport-logo" src={require('../images/beatport_2-256.png')} height="65px" />
-						<img className="soundcloud-logo" src={require('../images/soundcloud-logo.png')} height="65px" />
+						<a href="mailto:kiserr10@gmail.com"><img className="email-logo hvr-grow" src={require('../images/003-gmail-envelope.png')} height="65px" /></a>
+						<a href="https://www.linkedin.com/in/rosskiser/"><img className="linkedIn-logo hvr-grow" src={require('../images/005-linkedin-logo-1.png')} height="65px" /></a>
+						<a href="https://github.com/kiserr10"><img className="github-logo hvr-grow" src={require('../images/github-logo.png')} height="65px" /></a>
+						<a href="https://www.facebook.com/kiserr"><img className="facebook-logo hvr-grow" src={require('../images/002-facebook-black-social-button-circle.png')} height="65px" /></a>
+						<a href="https://www.instagram.com/kiserr/"><img className="insta-logo hvr-grow" src={require('../images/004-instagram-logo.png')} height="65px" /></a>
+						<a href="https://www.beatport.com/artist/ross-kiser/607262"><img className="beatport-logo hvr-grow" src={require('../images/beatport_2-256.png')} height="65px" /></a>
+						<a href="https://soundcloud.com/rosskiserofficial"><img className="soundcloud-logo hvr-grow" src={require('../images/soundcloud-logo.png')} height="65px" /></a>
 					</div>
 					<div className="contact-box-4">
-
+						<img className="contact-texture-image" src={require('../images/creation.png')} height="600px" />
 					</div>
 				</div>
 			</div>
